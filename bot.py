8ebe157ca270eaea2484168593e80337153f23b3
Abir Hasan <hackitabirhasan@gmail.com>
2021-04-24 19:03:39 +0600

    Removing Database & Broadcast!

--- bot.py
+++ bot.py
@@ -2,21 +2,12 @@
 
 import os
 import time
-import math
-import json
-import string
-import random
-import traceback
 import asyncio
-import datetime
-import aiofiles
-from random import choice
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, UsernameNotOccupied, ChatAdminRequired, PeerIdInvalid
 from configs import Config
-from database import Database
 
 ## --- Sub Configs --- ##
 BOT_USERNAME = Config.BOT_USERNAME
@@ -28,34 +19,12 @@ ABOUT_BOT_TEXT = Config.ABOUT_BOT_TEXT
 ABOUT_DEV_TEXT = Config.ABOUT_DEV_TEXT
 HOME_TEXT = Config.HOME_TEXT
 BOT_OWNER = Config.BOT_OWNER
-db = Database(Config.DATABASE_URL, BOT_USERNAME)
 broadcast_ids = {}
 Bot = Client(BOT_USERNAME, bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
 
-async def send_msg(user_id, message):
-	try:
-		await message.forward(chat_id=user_id)
-		return 200, None
-	except FloodWait as e:
-		await asyncio.sleep(e.x)
-		return send_msg(user_id, message)
-	except InputUserDeactivated:
-		return 400, f"{user_id} : deactivated\n"
-	except UserIsBlocked:
-		return 400, f"{user_id} : blocked the bot\n"
-	except PeerIdInvalid:
-		return 400, f"{user_id} : user id invalid\n"
-	except Exception as e:
-		return 500, f"{user_id} : {traceback.format_exc()}\n"
 
 @Bot.on_message(filters.command("start") & filters.private)
 async def start(bot, cmd):
-	if not await db.is_user_exist(cmd.from_user.id):
-		await db.add_user(cmd.from_user.id)
-		await bot.send_message(
-		    Config.LOG_CHANNEL,
-		    f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{BOT_USERNAME} !!"
-		)
 	usr_cmd = cmd.text.split("_")[-1]
 	if usr_cmd == "/start":
 		await cmd.reply_text(
@@ -128,75 +97,6 @@ async def main(bot, message):
 		except Exception as err:
 			print(f"Error: {err}")
 
-@Bot.on_message(filters.private & filters.command("broadcast") & filters.user(BOT_OWNER) & filters.reply)
-async def broadcast_(c, m):
-	all_users = await db.get_all_users()
-	broadcast_msg = m.reply_to_message
-	while True:
-	    broadcast_id = ''.join([random.choice(string.ascii_letters) for i in range(3)])
-	    if not broadcast_ids.get(broadcast_id):
-	        break
-	out = await m.reply_text(
-	    text = f"Broadcast Started! You will be notified with log file when all the users are notified."
-	)
-	start_time = time.time()
-	total_users = await db.total_users_count()
-	done = 0
-	failed = 0
-	success = 0
-	broadcast_ids[broadcast_id] = dict(
-	    total = total_users,
-	    current = done,
-	    failed = failed,
-	    success = success
-	)
-	async with aiofiles.open('broadcast.txt', 'w') as broadcast_log_file:
-	    async for user in all_users:
-	        sts, msg = await send_msg(
-	            user_id = int(user['id']),
-	            message = broadcast_msg
-	        )
-	        if msg is not None:
-	            await broadcast_log_file.write(msg)
-	        if sts == 200:
-	            success += 1
-	        else:
-	            failed += 1
-	        if sts == 400:
-	            await db.delete_user(user['id'])
-	        done += 1
-	        if broadcast_ids.get(broadcast_id) is None:
-	            break
-	        else:
-	            broadcast_ids[broadcast_id].update(
-	                dict(
-	                    current = done,
-	                    failed = failed,
-	                    success = success
-	                )
-	            )
-	if broadcast_ids.get(broadcast_id):
-	    broadcast_ids.pop(broadcast_id)
-	completed_in = datetime.timedelta(seconds=int(time.time()-start_time))
-	await asyncio.sleep(3)
-	await out.delete()
-	if failed == 0:
-	    await m.reply_text(
-	        text=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
-	        quote=True
-	    )
-	else:
-	    await m.reply_document(
-	        document='broadcast.txt',
-	        caption=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
-	        quote=True
-	    )
-	await os.remove('broadcast.txt')
-
-@Bot.on_message(filters.private & filters.command("status") & filters.user(BOT_OWNER))
-async def sts(c, m):
-	total_users = await db.total_users_count()
-	await m.reply_text(text=f"**Total Users in DB:** `{total_users}`", parse_mode="Markdown", quote=True)
 
 @Bot.on_callback_query()
 async def button(bot, cmd: CallbackQuery):
